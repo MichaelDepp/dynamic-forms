76f62f2e59904260347db38b3f7493efb183835f
MIchael Depp <michaeldepp7@gmail.com>
2021-09-08 13:29:12 +0800

    feat: created the modal to view the response and print the reponse

---
+++ src/components/ResponseModal/index.tsx
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import {
+  Stack,
+  Text,
+  Box,
+  Button,
+  useColorMode,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton
+} from "@chakra-ui/react";
+import _ from "lodash";
+import { Props } from "./props";
+
+const ResponseModal: React.FC<Props> = (prop) => {
+  const { isOpen, responseData, onClose } = prop;
+  const printRef = useRef<HTMLDivElement>(null);
+  const { colorMode } = useColorMode();
+  const bgColor = colorMode === "light" ? "#ffffff" : "#2c3848";
+
+  const onDownloadPdf = useReactToPrint({
+    content: () => printRef.current
+  });
+
+  return (
+    <Modal
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+      motionPreset="slideInBottom">
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{"Your Form Response 🌈"}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody ref={printRef} backgroundColor={bgColor}>
+          <Stack spacing={3}>
+            {responseData &&
+              _.map(responseData, (data, index) => {
+                return (
+                  <Box key={index}>
+                    <Text fontWeight="semibold" fontSize="xl">
+                      {data.name}
+                    </Text>
+                    <Text fontSize="lg">{data.value.toString()}</Text>
+                  </Box>
+                );
+              })}
+          </Stack>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onDownloadPdf}>
+            {"Download As PDF"}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ResponseModal;
