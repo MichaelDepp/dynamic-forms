047872cd5e73b403b35c75b48dbf377040ddd404
MIchael Depp <michaeldepp7@gmail.com>
2021-09-08 13:21:56 +0800

    feat: setup history page

---
+++ src/pages/history.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import FormRecord from "components/FormRecord";
+
+const History = () => {
+  return (
+    <Box mb={8} w="full">
+      <FormRecord />
+    </Box>
+  );
+};
+
+export default History;
