ca2c19751d5e9dfef1421063a8fc289b5562ce28
MIchael Depp <michaeldepp7@gmail.com>
2021-09-08 13:21:15 +0800

    feat: setup form page

---
+++ src/pages/form.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import FormViewer from "components/FormViewer";
+
+const Form = () => {
+  return (
+    <Box mb={8} w="full">
+      <FormViewer />
+    </Box>
+  );
+};
+
+export default Form;
