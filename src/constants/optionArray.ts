7c7029ba925d963520311fe51cc29e2db2f888b2
MIchael Depp <michaeldepp7@gmail.com>
2021-09-08 13:23:36 +0800

    feat: add the constant array for option

---
+++ src/constants/optionArray.ts
@@ -0,0 +1,26 @@
+export const optionsArr = [
+  {
+    name: "Text Input",
+    value: "textInput"
+  },
+  {
+    name: "Number Input",
+    value: "numberInput"
+  },
+  {
+    name: "Checkbox",
+    value: "checkbox"
+  },
+  {
+    name: "Boolean",
+    value: "boolean"
+  },
+  {
+    name: "Email",
+    value: "email"
+  },
+  {
+    name: "Date",
+    value: "date"
+  }
+];
