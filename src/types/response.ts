45d7d8945c446c02d336cb63956f9966e3f9fcad
MIchael Depp <michaeldepp7@gmail.com>
2021-09-08 13:23:10 +0800

    feat: define the response type

---
+++ src/types/response.ts
@@ -0,0 +1,12 @@
+export type response = {
+  value: string | boolean | number;
+};
+
+export type formResponseArr = {
+  formResponse: response[];
+};
+
+export type responseMap = {
+  name: string;
+  value: string | boolean | number;
+};
